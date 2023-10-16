69b54f890cbabc9d545126474691373ef909fb00
EvelynXD <91973757+EvelynXD@users.noreply.github.com>
2023-10-16 16:45:27 +0700

    Selasa.jsx

--- src/components/Mapel/Selasa.jsx
+++ src/components/Mapel/Selasa.jsx
@@ -1,13 +1,13 @@
 const Selasa = () => {
     return (
         <>
-	<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
+	<div className="border-t-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
 	   <div className="w-[50%] text-base  font-medium">Algoritma dan Dasar Pemrograman</div>
 	   <div className="flex justify-center items-center text-sm">08.00-09.40</div>
 	</div>
 	<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="1000">
-	   <div className="w-[50%] text-base font-medium">Lab. RPL</div>
-	   <div className="flex justify-center items-center text-sm">3 SKS</div>
+	   <div className="w-[50%] text-base font-medium text-sky-400">Lab. RPL</div>
+	   <div className="flex justify-center items-center text-sm text-sky-400/75">3 SKS</div>
 	</div>		
         </>
     )
