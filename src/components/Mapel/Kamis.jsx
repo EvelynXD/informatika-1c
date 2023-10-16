83a2d0d532bcc6d999fdc7ea98b324b087b0ea72
EvelynXD <91973757+EvelynXD@users.noreply.github.com>
2023-10-16 17:08:12 +0700

    Kamis.jsx

--- src/components/Mapel/Kamis.jsx
+++ src/components/Mapel/Kamis.jsx
@@ -5,7 +5,7 @@ const Kamis = () => {
 			<div className="w-[50%] text-base  font-medium">Pendidikan Literasi</div>
 			<div className="flex justify-center items-center text-sm">08.00-09.40</div>
 		</div>
-		<div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60 borde-b-2" data-aos="fade-up" data-aos-duration="600">
+		<div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60 border-b-2" data-aos="fade-up" data-aos-duration="600">
 			<div className="w-[50%] text-base  font-medium text-sky-400">H-203 Perikanan</div>
 			<div className="flex justify-center items-center text-sm text-sky-400/75">2 SKS</div>
 		</div>
