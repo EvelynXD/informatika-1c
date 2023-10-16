3f6517f7a546e35557c9d32209112d4ef26123ba
EvelynXD <91973757+EvelynXD@users.noreply.github.com>
2023-10-16 16:40:52 +0700

    Senin.jsx

--- src/components/Mapel/Senin.jsx
+++ src/components/Mapel/Senin.jsx
@@ -7,7 +7,7 @@ const Senin = () => {
 				</div>
 				<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="1000">
 					<div className="w-[50%] text-base font-medium text-sky-400">Lab. Multimedia</div>
-					<div className="flex justify-center items-center text-sm text-sky-400/25">3 SKS</div>
+					<div className="flex justify-center items-center text-sm text-sky-400/75">3 SKS</div>
 			</div>
         </>
     )
