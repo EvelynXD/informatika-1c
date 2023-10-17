57e54ff677238446a27eb2a543c2720acf37637b
EvelynXD <91973757+EvelynXD@users.noreply.github.com>
2023-10-17 08:58:32 +0700

    BoxOldWeb.jsx

--- src/components/BoxOldWeb.jsx
+++ src/components/BoxOldWeb.jsx
@@ -2,7 +2,7 @@
 const BoxOldWeb = () => {
 	return (
 		<div id="BoxOldWeb">
-			<a href="https://evelynxd-v2.vercel.app/">
+			<a href="https://evelynxd-portfolio-v2.vercel.app/">
 				<div className="flex justify-between">
 					<img src="/link.png" alt="" className="w-auto h-6" />
 					<img src="/next.png" alt="" className="h-3 w-3" />
